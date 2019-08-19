eedd6ff764a0f8bbb42ff00399328e7d085d6960
Lauren Rosenberg <laurenfay.rosenberg@mail.utoronto.ca>
2019-08-19 14:33:17 -0400

    Remove constant

--- MEDWebInterface/Constants.cs
+++ MEDWebInterface/Constants.cs
@@ -9,6 +9,5 @@ namespace MEDWebInterface
 {
     public static class Constants
     {
-        public static string ConnectionString = @"mongodb://middleegyptian:ZWnAcFRn6ZHocdcTly2HtmnebTwp6LB7Pcea9tF9oWmOJv18wXkrcLLbO3otjCQgotjtq6ZwV8hfKGrbwrjASw==@middleegyptian.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
     }
 }
